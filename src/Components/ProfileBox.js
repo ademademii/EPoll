f90b626f85148a8941bcf1695c745e64748c3a81
juriklatifi <jl53598@ubt-uni.net>
2024-06-21 19:16:10 +0200

    update ProfileBoxComponent.

--- src/Components/ProfileBox.js
+++ src/Components/ProfileBox.js
@@ -1,12 +1,34 @@
 // components/ProfileBox.js
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { useRouter } from 'next/router';
 import { jwtDecode } from 'jwt-decode';
 
-const ProfileBox = ({ user, onLogout }) => {
+const ProfileBox = ({ onLogout }) => {
     const router = useRouter();
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            try {
+                const decodedToken = jwtDecode(token);
+                const name = decodedToken['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'];
+                const role = decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
+
+                setUser({
+                    name: name,
+                    role: role
+                });
+            } catch (error) {
+                console.error("Failed to decode token:", error);
+                router.push('/login'); // Redirect to login page if token decoding fails
+            }
+        } else {
+            router.push('/login'); // Redirect to login page if token is not found
+        }
+    }, [router]);
 
     const handleLogout = () => {
         localStorage.removeItem('token');
@@ -15,47 +37,33 @@ const ProfileBox = ({ user, onLogout }) => {
         router.push('/login'); // Redirect to login page after logout
     };
 
-    // Function to handle navigation to survey form
     const handleGoToSurvey = () => {
-        router.push('/survey'); // Navigate to survey form page
-    };
-
-    // Check if user is defined and has the 'Admin' role
-    const isAdmin = user && user.role === 'Admin';
-    console.log(isAdmin)
-
-    // Decode token from localStorage to get user information if user is not defined
-    if (!user) {
-        const token = localStorage.getItem('token');
-        if (token) {
-            try {
-                const decodedToken = jwtDecode(token);
-                const name = decodedToken['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'];
-                const role = decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
-
-                user = {
-                    username: name,
-                    role: role
-                };
-            } catch (error) {
-                console.error("Failed to decode token:", error);
-                router.push('/login'); // Redirect to login page if token decoding fails
-            }
-        } else {
-            router.push('/login'); // Redirect to login page if token is not found
+        // Check current route to determine where to navigate
+        if (router.pathname === '/dashboard') {
+            router.push('/survey'); // Navigate to survey form page
+        } else if (router.pathname === '/survey') {
+            router.push('/dashboard'); // Navigate to dashboard page
         }
-    }
+    };
 
     return (
         <div className="bg-light p-3 shadow rounded text-dark" style={{ width: '300px' }}>
             <h4 className="mb-3">Profile</h4>
-            <p><strong>Username:</strong> {user.username}</p>
-            <p><strong>Role:</strong> {user.role}</p>
-            <Button variant="outline-danger" onClick={handleLogout}>Logout</Button>
-            {isAdmin && (
-                <Button variant="primary" className="mt-2" onClick={handleGoToSurvey}>
-                    Go to Survey Form
-                </Button>
+            {user ? (
+                <>
+                    <p><strong>Username:</strong> {user.name}</p>
+                    <p><strong>Role:</strong> {user.role}</p>
+                    <Button variant="outline-danger border-5" onClick={handleLogout} className="mb-2" block>
+                        Logout
+                    </Button> 
+                    {user.role === 'Admin' && (
+                        <Button variant="outline-primary  border-5" onClick={handleGoToSurvey} className='mb-2' block>
+                            {router.pathname === '/dashboard' ? 'Go to Survey Form' : 'Go to Dashboard'}
+                        </Button>
+                    )}
+                </>
+            ) : (
+                <p>Loading...</p>
             )}
         </div>
     );
