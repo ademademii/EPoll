67ba70e6f360b2c44e90204878faccf17f992ff7
Adem Ademi <ademiadem70@gmail.com>
2026-01-04 02:26:43 +0100

    Clean Arch

---
+++ ExitPoll.Infrastructure/Migrations/20240626190625_projectVote.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExitPoll.Migrations
+{
+    /// <inheritdoc />
+    public partial class projectVote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProjectId",
+                table: "Votes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Votes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProjectId",
+                table: "Votes");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Votes");
+        }
+    }
+}
